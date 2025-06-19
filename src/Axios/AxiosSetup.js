3530c6d495dd11778706c19f16d924d9c205bad3
Om Khot <iithomkhot@gmail.com>
2025-06-19 10:39:14 +0530

    socket config changed

--- src/Axios/AxiosSetup.js
+++ src/Axios/AxiosSetup.js
@@ -2,6 +2,10 @@ import axios from "axios";
 
 const baseURLCORS = import.meta.env.VITE_BACKEND_URL;
 
+if (!baseURLCORS) {
+  throw new Error("VITE_BACKEND_URL is not defined in production.");
+}
+
 
 const axiosInstance = axios.create({
     baseURL: baseURLCORS || "http://localhost:3000",
--- src/Utils/socketio.js
+++ src/Utils/socketio.js
@@ -1,5 +1,8 @@
 import {io} from "socket.io-client";
 
-const socket = io("http://localhost:3000");
+const socket = io("https://connectme-backend-2-h4co.onrender.com", {
+    transports: ["websocket"],
+    withCredentials: true
+});
 
 export default socket;
